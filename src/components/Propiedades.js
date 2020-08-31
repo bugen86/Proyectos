442eb04b414568dfad9d1128f6eeca4d125cc7f2
ezequielpato <ezequiel.salomon@gmail.com>
2020-08-30 22:00:28 -0300

    Se crea componente Propiedades

---
+++ src/components/Propiedades.js
@@ -0,0 +1,18 @@
+// React
+import React, { Component } from "react"
+
+// Css
+import "./css/Propiedades.css"
+
+// Main
+class Propiedades extends Component {
+	render() {
+		return (
+			<main className="Propiedades">
+				<h3>Propiedades</h3>
+			</main>
+		)
+	}
+}
+
+export default Propiedades
