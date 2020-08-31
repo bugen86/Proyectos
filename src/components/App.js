18414b54874eb4a81bb0e902c477127eb676f6e5
ezequielpato <ezequiel.salomon@gmail.com>
2020-08-31 00:33:40 -0300

    Se implementa Switch y Router. Nuevos componentes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 // React
 import React, { Component } from "react"
+import { Switch, Route } from "react-router-dom"
 
 // Css
 import "./css/App.css"
@@ -7,8 +8,9 @@ import "./css/App.css"
 // Components
 import Header from "./Header"
 import Main from "./Main"
+import Propiedades from "./Propiedades"
 import Footer from "./Footer"
-import NavBarLink from "./NavBar/NavBarLink/NavBarLink"
+// import NavBarLink from "./NavBar/NavBarLink/NavBarLink"
 
 // Main
 class App extends Component {
@@ -16,8 +18,11 @@ class App extends Component {
 		return (
 			<React.Fragment>
 				<Header />
-				<NavBarLink donde="https://google.com" texto="Apretame" />
-				<Main />
+				{/* <NavBarLink donde="https://google.com" texto="Apretame" /> */}
+				<Switch>
+					<Route exact path="/" component={Main} />
+					<Route path="/propiedades" component={Propiedades} />
+				</Switch>
 				<Footer />
 			</React.Fragment>
 		)
