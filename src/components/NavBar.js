18ece870733b8b42309595e14f96eadfe159866e
ezequielpato <ezequiel.salomon@gmail.com>
2020-08-30 21:59:54 -0300

    Se crea componente NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+// React
+import React, { Component } from "react"
+
+// Css
+import "./css/NavBar.css"
+
+// Main
+class NavBar extends Component {
+	render() {
+		return (
+			<div className="NavBar">
+				<h4>home</h4>
+			</div>
+		)
+	}
+}
+
+export default NavBar
